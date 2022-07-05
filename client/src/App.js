929c105d6297145a0723fe8da72a34d53747105f
Grayd-WebDev <grayd2@yandex.ru>
2022-07-06 00:26:36 +0300

    Setting up the starting template

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
+import Login from "./components/pages/Login";
+import Restaurants from "./components/pages/Restaurants";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Restaurants />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <h1>HEllo guys</h1>
     </div>
   );
 }
---
+++ client/src/components/RestaurantsList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RestaurantsList = () => {
+  return <div>Restaurants-list</div>;
+};
+
+export default RestaurantsList;
